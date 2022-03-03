5d08373ba4dbf87e8e2174e09b3ebc6dbeaa6a00
Alvin Cheng <alvincheng@alvins-mbp.lan>
2022-03-02 19:37:55 -0800

    make it better

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+const url = 'https://api.pactsafe.com/v1.1/sites/16489/signers/id:alvin.cheng%40ironcladapp.com/activity?expand=contract,version&filter=event_type==agreed';
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: 'Bearer kH7qqXGW5kt5XFoX59fI0CH8WnkTLbrlGDdiX9G-Ut8_'
+  }
+};
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+
+var obj;
+
+fetch(url, options)
+      .then(res => res.json())
+      .then(data => obj = data)
+      .then(json => console.log(obj))
+      .catch(err => console.error('error:' + err));
+
+app.get('/api', (request, response) => {
+  response.json({ obj });
+});
+
+
+
